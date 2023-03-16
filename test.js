872c3154b1601b59204162ce83623bf53a8071a6
rrisaac <rrisaac@up.edu.ph>
2023-03-17 01:12:06 +0800

    feat: new function to addAccount

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+
+// Export style used is Commonjs
 const functions = require("./functions.js");
 
 console.log(functions.generateUniqueID("Alan", "Turing"));
+
+console.log(functions.addAccount(["Alan","Turing","aturing@w3c.com",58]));
