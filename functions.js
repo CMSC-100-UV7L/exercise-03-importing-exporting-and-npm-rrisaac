b73d2fe526f2a28ad140fb3ba67a1e0d9d1839df
rrisaac <rrisaac@up.edu.ph>
2023-03-17 00:23:24 +0800

    feat: new function to generate unique ID

--- functions.js
+++ functions.js
@@ -1,5 +1,15 @@
-const generateUniqueID = (firstName, lastName) => {
-    return firstName + lastName;
-}
+const { v4: uuidv4 } = require('uuid');
 
-export default generateUniqueID
+// This function accepts first and last name and generates a unique ID
+const generateUniqueID = (firstName, lastName) => {
+    
+    const uuid = uuidv4().split("-")[0]; // generate uuid using uuid package then trim first delimited id consisting of 8 characters
+
+    const final = ""; // initialize empty string for readability
+
+    // returns uuid consisting of lowercase first character of first name, last name, and the manipulated uuid
+    return final.concat(firstName.charAt(0).toLowerCase(), lastName.toLowerCase(), uuid)
+};
+
+
+module.exports.generateUniqueID = generateUniqueID;
---
+++ test.js
@@ -0,0 +1,3 @@
+const functions = require("./functions.js");
+
+console.log(functions.generateUniqueID("Alan", "Turing"));
